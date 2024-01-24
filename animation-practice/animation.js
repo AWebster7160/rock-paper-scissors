c2881d0c433f6c7ce8b54283759741f236d2bcdf
AWebster7160 <156373395+AWebster7160@users.noreply.github.com>
2024-01-24 16:42:23 -0500

    Add opponent bottom hand to other side of screen

--- animation-practice/animation.js
+++ animation-practice/animation.js
@@ -1,9 +1,16 @@
 const img = document.querySelector('#wantToSpin');
-
+const imgRight = document.querySelector('#wantToSpinToo');
 const handHit = [
     { transform: 'rotate(0turn)'},
     {transform: 'rotate(.03turn', easing: 'ease-out', offset: '.17'},
     {transform: 'rotate(-.01turn'},
+    { transform: 'rotate(0turn)'},
+];
+
+const handHitMirror = [
+    { transform: 'rotate(0turn)'},
+    {transform: 'rotate(-.03turn', easing: 'ease-out', offset: '.17'},
+    {transform: 'rotate(.01turn'},
     { transform: 'rotate(0turn)'},
 ];
 const spinTime = {
@@ -13,5 +20,6 @@ const spinTime = {
 window.addEventListener('keyup', (event) => {
     if (event.key === ' ') {
     img.animate(handHit, spinTime);
+    imgRight.animate(handHitMirror, spinTime);
     };
 });
