563425574ef6ae133e93f61d8012cd12609f3707
AWebster7160 <156373395+AWebster7160@users.noreply.github.com>
2024-01-25 11:51:59 -0500

    Add player fist and position correctly on page

--- animation-practice/animation.js
+++ animation-practice/animation.js
@@ -1,5 +1,12 @@
-const img = document.querySelector('#wantToSpin');
-const imgRight = document.querySelector('#wantToSpinToo');
+const img = document.querySelector('#player-palm');
+const imgRight = document.querySelector('#computer-palm');
+const playerFist = document.querySelector('#player-fist');
+const raiseHand = [
+    {transform: 'rotate(0)'},
+    {transform: 'rotate(-.08turn)', offset: '.70'},
+    {transform: 'rotate(0'}
+]
+
 const handHit = [
     { transform: 'rotate(0turn)'},
     {transform: 'rotate(.03turn', easing: 'ease-out', offset: '.17'},
@@ -13,13 +20,25 @@ const handHitMirror = [
     {transform: 'rotate(.01turn'},
     { transform: 'rotate(0turn)'},
 ];
-const spinTime = {
-    duration: 800
+const palmTime = {
+    duration: 800,
+}
+
+const fistTime = {
+    duration: 550,
 }
 
 window.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        playerFist.animate(raiseHand, fistTime);
+    }
+})
+
+window.addEventListener('keyup', (event) => {
     if (event.key === ' ') {
-    img.animate(handHit, spinTime);
-    imgRight.animate(handHitMirror, spinTime);
+    img.animate(handHit, palmTime,);
+    imgRight.animate(handHitMirror, palmTime);
+    
     };
+
 });
